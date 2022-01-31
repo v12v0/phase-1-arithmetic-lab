91258c78cbb4703ccebd73a3ec8b9c61cf9acf7a
Vi Vo <aaronvo118@gmail.com>
2022-01-31 13:22:52 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() > 0)
+
+const num3 = 34
+const num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(20)
